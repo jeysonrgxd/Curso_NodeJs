16dd0867c1c9ce5cad2017f28d3187ae30abb880
jeyson <jeysonrgxd@gmail.com>
2021-01-13 01:43:07 -0500

    terminando clase socket basico

--- 7-sockets-basico/server/sockets/socket.js
+++ 7-sockets-basico/server/sockets/socket.js
@@ -17,23 +17,30 @@ module.exports.serverSocket = function (server) {
 
       // escuchamos el evento que nos manda el cliente y recibimos su informacion mediante el parametro de nuestro callback
       // con el segundo paramentro callback, podemos enviarle al cliente si algo salio mal por ejemplo si queremos guardar la data en la base de datos y tenemos un error ps enviamos el callback con un mensaje de error, ya el cliente lo recivira mediante su callback
-      client.on("enviarMensaje", (message, callback) => {
-         console.log(message);
-         if (message.usuario) {
-            callback("Tiene usuario asi que TODO SALIO BIEN!")
-         }
-         else {
-            callback("No tiene usuario asi que TODO SALIO MAL")
+      client.on("enviarMensaje", (data, callback) => {
+         // console.log(data);
+         // if (data.usuario) {
+         //    callback("Tiene usuario asi que TODO SALIO BIEN!")
+         // }
+         // else {
+         //    callback("No tiene usuario asi que TODO SALIO MAL")
 
-         }
+         // }
+
+
+         // emitimos a todos los conectados, pero lo hacemos en el evento enviarMensaje ya que es el que uno de los clientes emitio y el cual lo capturamos y lo mandamos a todos la misma data que nos envia cualquier cliente
+         client.broadcast.emit("enviarMensaje",data)
       })
 
       // emitimos un mensaje para el cliente (le mandamos info al cliente), lo ponemos dentro de un setTimeout para que se demore un poquito
       setTimeout(() => {
+         // emitimos desde el servidor alos clientes
          client.emit("enviarMensaje", {
             usuario: "Administrador",
             message: "Bienvenido a esta aplicacion"
          })
+         
+
       }, 1000)
 
 
